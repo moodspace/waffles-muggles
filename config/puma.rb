6dbf233de7f4cea606a214ad742ae45fc93cbca5
moodspace <moodspace@outlook.com>
2017-03-21 19:24:53 -0400

    heroku prep

--- config/puma.rb
+++ config/puma.rb
@@ -7,7 +7,7 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests, default is 3000.
+# Specifies the `port` that Puma will listen on to receive requests, default is 8000.
 #
 port        ENV.fetch("PORT") { 8000 }
 
