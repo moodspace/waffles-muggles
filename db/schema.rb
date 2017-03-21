5d7f627448489c429095a9a0f92512aa9cb8c858
moodspace <moodspace@outlook.com>
2017-03-21 16:40:17 -0400

    fix db migration error

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,5 +11,68 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 0) do
+
+  create_table "errors", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.integer "code"
+    t.string "message"
+    t.string "fields"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "floors", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.integer "id"
+    t.string "name"
+    t.decimal "size_x", precision: 10
+    t.decimal "size_y", precision: 10
+    t.string "geojson"
+    t.integer "library"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "libraries", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.integer "id"
+    t.string "name"
+    t.decimal "latitude", precision: 10
+    t.decimal "longitude", precision: 10
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rules", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.string "rule_type"
+    t.integer "rule_id"
+    t.string "call_number"
+    t.string "rule"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "search_results", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.string "result_type"
+    t.integer "result_id"
+    t.string "result"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stacks", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.integer "id"
+    t.decimal "cx", precision: 10
+    t.decimal "cy", precision: 10
+    t.decimal "lx", precision: 10
+    t.decimal "ly", precision: 10
+    t.decimal "rotation", precision: 10
+    t.string "geojson"
+    t.string "start_class"
+    t.integer "start_subclass"
+    t.string "end_class"
+    t.integer "end_subclass"
+    t.integer "oversize"
+    t.integer "floor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
